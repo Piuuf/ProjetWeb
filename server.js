f270309d86d2cbcc960c4be7f39c9e459109f7ba
Piuuf <benjaminhenry@viacesi.fr>
2018-04-13 16:38:08 +0200

    Ajout majeur

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
+var express = require('express');
+var app = express();
+var path = require('path');
 
+app.get('/', function(req,res) {
+app.use(express.static('css'));
+  res.sendfile('index.html');
+
+});
+
+app.listen(8080);
